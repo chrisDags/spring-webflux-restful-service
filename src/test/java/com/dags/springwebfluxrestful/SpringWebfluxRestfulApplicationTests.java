00c6b27244f4ad0846d9ca774544c592ea965b9e
Chris <christopher-dags@hotmail.com>
2021-03-17 13:08:56 -0400

    initial commit

---
+++ src/test/java/com/dags/springwebfluxrestful/SpringWebfluxRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dags.springwebfluxrestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxRestfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
